7e24597b3415f8715be361cfba9491e8c9f2212e
YESsPeace <97120170+YESsPeace@users.noreply.github.com>
2023-04-21 20:53:13 +0400

    AccountsMenu is now created with RecycleView

--- AppMenus/Accounts_menu/AccountsMenu_main.py
+++ AppMenus/Accounts_menu/AccountsMenu_main.py
@@ -1,12 +1,78 @@
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.metrics import dp
-from kivymd.uix.button import MDRaisedButton
+from kivy.properties import DictProperty, NumericProperty, StringProperty
+from kivymd.uix.anchorlayout import MDAnchorLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDRaisedButton, MDIconButton
+from kivymd.uix.card import MDCard
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
 
 import config
 from database import accounts_db_read, savings_db_read
+
+
+class AccountsItem(MDCard):
+    radius = [0, 0, 0, 0]
+    padding = [dp(5), dp(5), dp(5), dp(5)]
+    md_bg_color = [0.12941176470588237, 0.12941176470588237, 0.12941176470588237, 1.0]
+    spacing = dp(10)
+    ripple_behavior = True
+
+    account_data = DictProperty()
+    account_id = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        Clock.schedule_once(self.set_widget_props)
+
+    def set_widget_props(self, *args):
+        account_data = self.account_data
+
+        self.add_widget(
+            MDAnchorLayout(
+                MDIconButton(
+                    icon=str(account_data['Icon']),
+                ),
+                size_hint_x=None,
+                width=dp(60),
+                padding=dp(20),
+                md_bg_color=account_data['Color'],
+            )
+        )
+
+        self.add_widget(
+            MDLabel(
+                text=str(account_data['Name'])
+            )
+        )
+
+        self.add_widget(
+            MDLabel(
+                text=str(account_data['Balance']),
+                halign='right'
+            )
+        )
+
+
+class type_label(MDBoxLayout):
+    padding = [dp(5), dp(5), dp(5), dp(5)]
+    md_bg_color = [0.12941176470588237, 0.12941176470588237, 0.12941176470588237, 1.0]
+
+    text = StringProperty()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        Clock.schedule_once(self.set_widget_props)
+
+    def set_widget_props(self, *args):
+        self.add_widget(
+            MDLabel(
+                text=str(self.text)
+            )
+        )
 
 
 class AccountsMenu_main(MDScreen):
@@ -20,71 +86,50 @@ class AccountsMenu_main(MDScreen):
         print("# accounts_data_dictionary:", self.accounts_data_dict)
         print('# savings_data_dictionary:', self.savings_data_dict)
 
-        Clock.schedule_once(self.adding_accountsANDsavings_main_menu, 0)
+        Clock.schedule_once(self.set_widget_props)
 
-    def adding_accountsANDsavings_main_menu(self, *args):
-        accounts_amount, savings_amount = 0, 0
+    def set_widget_props(self, *args):
+        Clock.schedule_once(self.set_accounts)
 
-        for account_id in self.accounts_data_dict:
-            my_widget = MDRaisedButton(
-                id=str(account_id),
-                # icon='Icons/Btn_icon/Accounts-icon-white.png',
-                text=self.accounts_data_dict[account_id]['Name'],
-                text_color='white',
-                height=dp(75),
-                md_bg_color=self.accounts_data_dict[account_id]['Color'],
-                size_hint=(1, 1), halign='left',
-                on_release=self.open_menu_for_new_account
+    def set_accounts(self, *args):
+        accounts, savings = accounts_db_read(), savings_db_read()
+        self.ids.accounts_rv.data = []
+
+        for type_name, data_dict in [('Accounts', accounts), ('Savings', savings)]:
+            self.ids.accounts_rv.data.append(
+                {
+                    "viewclass": "type_label",
+                    "height": dp(40),
+                    "orientation": "horizontal",
+                    "text": type_name,
+                }
             )
 
-            my_widget.add_widget(
-                MDLabel(
-                    text=str(self.accounts_data_dict[account_id]['Balance']) + ' ' +
-                         self.accounts_data_dict[account_id]['Currency'],
-                    halign='right'
+            for account_id in data_dict.keys():
+                account_data = data_dict[account_id]
+
+                self.ids.accounts_rv.data.append(
+                    {
+                        "viewclass": "AccountsItem",
+                        "height": dp(50),
+                        "orientation": "horizontal",
+                        "account_data": account_data,
+                        "account_id": account_id,
+                        "on_release": self.on_release_callback(account_id)
+                    }
                 )
-            )
 
-            self.ids.accounts_Boxlines.add_widget(my_widget)
-            if self.accounts_data_dict[account_id]['Currency'] == 'RUB':
-                accounts_amount += float(self.accounts_data_dict[account_id]['Balance'])
+    def on_release_callback(self, account_id):
+        return lambda: self.open_menu_for_new_account(account_id)
 
-        self.ids.accounts_amount.text = str(accounts_amount)
+    def open_menu_for_new_account(self, account_id, *args):
+        print(account_id)
 
-        for savings_id in self.savings_data_dict:
-            my_widget = MDRaisedButton(
-                id=str(savings_id),
-                # icon='Icons/Btn_icon/Accounts-icon-white.png',
-                text=self.savings_data_dict[savings_id]['Name'],
-                text_color='white',
-                md_bg_color=self.savings_data_dict[savings_id]['Color'],
-                size_hint=(1, 1), halign='left',
-                on_release=self.open_menu_for_new_account
-            )
+        self.account_info = (accounts_db_read() | savings_db_read())[account_id]
 
-            my_widget.add_widget(
-                MDLabel(
-                    text=str(self.savings_data_dict[savings_id]['Balance']) + ' ' +
-                         self.savings_data_dict[savings_id]['Currency'],
-                    halign='right'
-                )
-            )
-
-            self.ids.savings_Boxlines.add_widget(my_widget)
-
-            if self.savings_data_dict[savings_id]['Currency'] == 'RUB':
-                savings_amount += float(self.savings_data_dict[savings_id]['Balance'])
-
-        self.ids.savings_amount.text = str(savings_amount)
-
-    def open_menu_for_new_account(self, button, *args):
-        print(button.id)
-
-        self.account_info = (accounts_db_read() | savings_db_read())[button.id]
-
-        self.account_info['ID'] = button.id
+        self.account_info['ID'] = account_id
         self.account_info['new'] = False
-        self.account_info['type'] = 'savings' if button.id.split('_')[0] == 'savings' else 'regular'
+        self.account_info['type'] = 'savings' if account_id.split('_')[0] == 'savings' else 'regular'
 
         config.account_info = self.account_info
 
