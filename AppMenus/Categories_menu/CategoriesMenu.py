7815d02dd6d1bb746b4f0eed0539e1a2a4195d4f
YESsPeace <97120170+YESsPeace@users.noreply.github.com>
2023-04-26 15:39:03 +0400

    Fixed bug with top_bar of CategoriesMenu in edit mode

--- AppMenus/Categories_menu/CategoriesMenu.py
+++ AppMenus/Categories_menu/CategoriesMenu.py
@@ -3,7 +3,7 @@ from copy import copy
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.metrics import dp
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, BooleanProperty
 from kivy.uix.screenmanager import NoTransition, SlideTransition
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDIconButton
@@ -19,6 +19,7 @@ from database import categories_db_read, incomes_db_read
 
 class CategoriesMenu(MDScreen):
     total_accounts_balance = NumericProperty(get_total_accounts_balance())
+    editing_mode = BooleanProperty(False)
 
     def __init__(self, *args, **kwargs):
 
@@ -27,43 +28,18 @@ class CategoriesMenu(MDScreen):
         self.current_menu_month_name = config.current_menu_month_name
         self.days_in_month_icon_dict = config.days_in_month_icon_dict
         self.days_in_current_menu_month = config.days_in_current_menu_month
+        self.edit_button_on_release = self.start_edit_mode
 
         super().__init__(*args, **kwargs)
-
-        Clock.schedule_once(self.set_widget_props, 1)
-
-    def set_widget_props(self, *args):
-
-        self.ids['total_balance_label'] = MDLabel(
-            id='total_balance_label',
-            text=str(self.total_accounts_balance),
-            halign="center",
-            valign="middle",
-        )
-
-        self.ids['total_balance_label'].font_size = "26sp"
-
-        self.top_btn_bar = MDBoxLayout(
-            MDIconButton(
-                icon='menu',
-                size_hint_y=1,
-            ),
-            self.ids['total_balance_label'],
-            MDIconButton(
-                icon='pencil',
-                size_hint_y=1,
-                on_release=self.start_edit_mode,
-            ),
-            orientation='horizontal',
-            size_hint_y=None,
-            height=dp(60),
-            md_bg_color=[.6, .1, .2, 1],
-        )
 
     def update_total_accounts_balance(self, *args):
         self.ids.total_balance_label.text = str(get_total_accounts_balance())
 
     def load_previous_month(self):
+        if self.editing_mode is True:
+            self.editing_mode = False
+            self.quit_from_edit_mode()
+
         load_previous_month(self, Categories_buttons_menu)
 
         name_ = str(config.current_menu_date)[:-3]
@@ -74,6 +50,10 @@ class CategoriesMenu(MDScreen):
         self.ids.incomes_swiper.current = name_
 
     def load_next_month(self):
+        if self.editing_mode is True:
+            self.editing_mode = False
+            self.quit_from_edit_mode()
+
         load_next_month(self, Categories_buttons_menu)
 
         name_ = str(config.current_menu_date)[:-3]
@@ -85,33 +65,11 @@ class CategoriesMenu(MDScreen):
 
     def start_edit_mode(self, *args):
         print('# start edit mode')
+        self.editing_mode = True
 
-        # switch top_bar to edit mode
-        self.month_menu = copy(self.ids.month_menu)
-
-        self.ids.top_bar.clear_widgets()
-        self.ids.top_bar.height = dp(48)
-
-        edit_mode_top_bar = \
-            MDBoxLayout(
-                MDBoxLayout(
-                    MDIconButton(
-                        icon='arrow-left',
-                        on_release=self.quit_from_edit_mode
-                    ),
-                    MDLabel(
-                        text='Редактирование',
-                        halign='left',
-                    ),
-                    orientation='horizontal',
-                    md_bg_color=(.2, .4, .85, 1),
-                ),
-                orientation='vertical',
-            )
-
-        self.ids.top_bar.add_widget(
-            edit_mode_top_bar
-        )
+        self.ids.top_bar.md_bg_color = [.1, .2, .8, 1]
+        self.edit_button_on_release = self.quit_from_edit_mode
+        self.ids.edit_button.icon = 'close'
 
         # rebind buttons functions
         for swiper_id, rv_id, plus_button_id in [
@@ -143,12 +101,11 @@ class CategoriesMenu(MDScreen):
 
     def quit_from_edit_mode(self, *args):
         print('# quit from edit mode')
-        self.ids.top_bar.height = dp(100)
+        self.editing_mode = False
 
-        self.ids.top_bar.clear_widgets()
-
-        self.ids.top_bar.add_widget(self.top_btn_bar)
-        self.ids.top_bar.add_widget(self.month_menu)
+        self.ids.top_bar.md_bg_color = [.6, .1, .2, 1]
+        self.edit_button_on_release = self.start_edit_mode
+        self.ids.edit_button.icon = 'pencil'
 
         # rebind buttons functions
         for swiper_id, rv_id in [('my_swiper', 'Categories_rv'), ('incomes_swiper', 'Incomes_rv')]:
@@ -156,7 +113,6 @@ class CategoriesMenu(MDScreen):
 
     def open_menu_for_edit_categories(self, category_id, *args):
         self.quit_from_edit_mode()
-
         if category_id in ['plus_button_categories', 'plus_button_incomes']:
             config.category_item = {
                 'ID': None,
